8bdc6260682ac6c3e204605a91449aab6f18202a
alpetrovich <alexispetrovich11@gmail.com>
2022-05-30 10:18:10 -0300

    [add] order-service and create order

---
+++ order-service/src/main/java/com/microservice/model/Order.java
@@ -0,0 +1,24 @@
+package com.microservice.model;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "t_orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList;
+}
