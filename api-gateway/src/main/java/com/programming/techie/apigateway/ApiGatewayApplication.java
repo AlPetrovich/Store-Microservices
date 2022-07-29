ab0941ef08dd71745e58863b81d446f6faaccf0e
alpetrovich <alexispetrovich11@gmail.com>
2022-07-29 12:19:59 -0300

    [add] security OAuth2 keycloak

--- api-gateway/src/main/java/com/programming/techie/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/programming/techie/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,5 @@
-package com.programming.apigateway;
+package com.programming.techie.apigateway;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
