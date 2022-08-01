2fc4772d9a7f7f10e78401cedd58c3bc34454653
alpetrovich <alexispetrovich11@gmail.com>
2022-08-01 16:24:31 -0300

    Circuit Breaker pattern

--- inventory-service/src/main/java/com/inventory/service/service/InventoryService.java
+++ inventory-service/src/main/java/com/inventory/service/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.inventory.service.service;
 import com.inventory.service.dto.InventoryResponseDTO;
 import com.inventory.service.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,14 +12,20 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     //devuelve una lista
     @Transactional(readOnly = true) //solo de lectura
+    @SneakyThrows
     public List<InventoryResponseDTO> isInStock(List<String> skuCode) {
         //objetos de inventario que coincidan con el codigo
+        log.info("Wait Started");
+        //simulacion ejecucion tarda 10 segundos
+        Thread.sleep(10000);
+        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode)
                 .stream()
                 .map(inventory ->
